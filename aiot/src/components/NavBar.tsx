bcc3d2b594b95cb9274d583dd5a7dd8e47da317b
Sarvesh <sarveshjaiofficial@gmail.com>
2024-09-14 05:48:19 +0530

    Implemented Nav Links

--- aiot/src/components/NavBar.tsx
+++ aiot/src/components/NavBar.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <nav className="pt-12">
       <div className="flex items-center justify-between px-24">
         {/* Logo Div */}
-        <div className="flex gap-2">
-          <Image alt="Logo" src={"/favicon.svg"} height={34} width={35} />
-          <h1 className="font-bold text-2xl">AIOT</h1>
-        </div>
+        <Link href={"/"}>
+          <div className="flex gap-2">
+            <Image alt="Logo" src={"/favicon.svg"} height={34} width={35} />
+            <h1 className="font-bold text-2xl">AIOT</h1>
+          </div>
+        </Link>
 
         {/* Nav Links */}
         <div className="flex gap-16 px-3 pl-20">
-          <a href="">Home</a>
-          <a href="">Saved Destinations</a>
-          <a href="">Translate</a>
-          <a href="">Forex</a>
+          <Link href="/">Home</Link>
+          <Link href="">Saved Destinations</Link>
+          <Link href="/translate">Translate</Link>
+          <Link href="">Forex</Link>
         </div>
 
         {/* Auth */}
